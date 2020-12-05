d9d0ded91a629033e5014ac2a12709b8b01d8a9f
weiyap <tanweiyap@gmail.com>
2020-12-05 12:08:37 +0100

    Version number update

--- DocCompareWPF/MainWindow.xaml.cs
+++ DocCompareWPF/MainWindow.xaml.cs
@@ -77,7 +77,7 @@ namespace DocCompareWPF
     {
         private readonly string appDataDir = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".2compare");
         private readonly DocumentManagement docs;
-        private readonly string versionString = "Version 0.5.5";
+        private readonly string versionString = "Version 0.5.6";
         private readonly string workingDir = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".2compare");
         private string compareResultFolder;
         private bool docCompareRunning, docProcessRunning, animateDiffRunning, showMask;

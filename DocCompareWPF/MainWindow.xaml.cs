e5433e1db56f3eaf705c2130c8a138542e1ce4bb
weiyap <tanweiyap@gmail.com>
2021-02-16 20:17:48 +0100

    updated reference document heading to be shorter

--- DocCompareWPF/MainWindow.xaml.cs
+++ DocCompareWPF/MainWindow.xaml.cs
@@ -77,7 +77,7 @@ namespace DocCompareWPF
     {
         private readonly string appDataDir = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".2compare");
         private readonly DocumentManagement docs;
-        private readonly string versionString = "1.0.7";
+        private readonly string versionString = "1.0.9";
         private readonly string localetype = "EN";
         private readonly string workingDir = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".2compare");
         private string compareResultFolder;
